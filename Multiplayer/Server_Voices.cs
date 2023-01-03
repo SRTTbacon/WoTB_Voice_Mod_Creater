4c86604399e6ed88e1df975e9cae57a76b6c8d4a
SRTTbacon <54164333+SRTTbacon@users.noreply.github.com>
2023-01-03 11:17:59 +0900

    Delete Multiplayer directory

--- Multiplayer/Server_Voices.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace WoTB_Voice_Mod_Creater.Multiplayer
-{
-    public class Server_Voices
-    {
-        public static List<string> Voice_List = new List<string>();
-    }
-}
